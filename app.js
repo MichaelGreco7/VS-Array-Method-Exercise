b14ec154a7a13d9a4c34a655c40a7348e0315792
Michael Greco <michael.greco07@gmail.com>
2020-07-07 15:13:57 -0400

    step 2

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ var fruit = ['banana', 'apple', 'orange', 'watermelon'];
 var vegetables = ['carrot', 'tomato', 'pepper', 'lettuce'];
 
 // ! Remove the last item from the vegetable array.
-vegetables.pop();
-console.log(vegetables);
+
+// vegetables.pop();
+// console.log(vegetables);
+
+// ! Remove the first item from the fruit array.
+
+fruit.shift();
+console.log(fruit);
